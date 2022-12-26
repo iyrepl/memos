c955d9134b7c6f2152091bac680d71af595e25f8
Git IY! <91715524+gitiy1@users.noreply.github.com>
2022-12-26 16:08:31 +0800

    Update glitch.sh

--- glitch.sh
+++ glitch.sh
@@ -1,7 +1,6 @@
 if [ ! -f "memos" ];then
-  #download memos lastest
-  curl -L https://github.com/gitiy1/memos/releases/latest/download/memos
+  wget -c https://github.com/gitiy1/memos/releases/latest/download/memos
   chmod 777 memos
-  
-#启动memos
+fi
+
 ./memos --mode prod --port 3000
